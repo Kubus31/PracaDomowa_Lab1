b9a25a774bae50360bda4edffeb7417624e21086
Kubus31 <jakubbazelak@gmail.com>
2022-03-03 21:08:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+
+import math
+
+# zadanie1
+
+x = 1
+y = 2
+z = 3
+zz = 4
+
+print(x+y)
+
+# zadanie 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(z//y)
+print(zz % y)
+print(z**y)
+print(math.sqrt(zz))
+print("koniec zadania")
+
+# zadanie 3
+
+
+a = 10
+a += 5
+
+b = 6
+b -= 3
+
+c = 20
+c *= 10
+
+
+d = 20
+d **= 2
+
+e = 5
+e /= 3
+
+f = 13
+f %= b
+print(a, b, c, d, e, f)
+
+#zadanie 4
+
+print(math.e**10)
+print(math.pow(math.log(5+math.pow(math.sin(8), 2), math.e), 6))
+print(math.floor(3.55))
+print(math.ceil(4.80))
+
+#zadanie 5
+
+Imie = "jakub"
+Nazwisko = "bazelak"
+
+print(Imie.capitalize(), Nazwisko.capitalize())
+
+#zadanie 6
+
+tekstPiosenki = "la la la la la "
+print(tekstPiosenki.count("la"))
+
+#zadanie 7
+
+ciagZnakow = "Witaj Swiecie!"
+
+print(ciagZnakow[1], ciagZnakow[12])
+
+#zadanie 8
+
+nastepnyTekstPiosenki = tekstPiosenki.split(" ")
+
+print(nastepnyTekstPiosenki)
+
+#zadanie 9
+
